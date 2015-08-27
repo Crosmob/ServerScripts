9440ce4938537208353051cc64e3c57ee50e3f63
Evangelos Brachos <ebrachos@gmail.com>
2015-08-27 09:29:00 +0200

    add server check

--- eai/wmq/find_queues.pl
+++ eai/wmq/find_queues.pl
@@ -6,17 +6,22 @@ use Sys::Hostname;
 my $host = hostname();
 my $display = "";
 my $dspmq = "";
+my $qm = "";
+my $qmstatus = "";
 # check if there is mq server installation
 my $dspmqpath = "/opt/mqm/bin/dspmq";
 my $dspmqverpath= "/opt/mqm/bin/dspmqver";
 
 if(-e $dspmqpath && -e $dspmqverpath){
-        # mq server installation was found - now we will find the running qmgrs
+	# mq server installation was found - now we will find the running qmgrs 
 	$dspmq = `dspmq`;
 	my @qmgrs = split /\n/, $dspmq;
 	foreach my $dspmqline (@qmgrs) {
 		if($dspmqline =~ m/\s*QMNAME\(([\w\.]+)\)\s+STATUS\((\w+)\)/) {
-			print "$1,$2\n";
+			$qm = "$1";
+			$qmstatus = "$2";
+			# print "$1,$2\n";
+			# go through the running queue managers and get the queues info
 			if( $2 eq "Running"){
 				$display = `echo 'DIS Q(*) TYPE' | runmqsc $1`;
 				my @resultLines = split /\n/, $display;
@@ -36,8 +41,6 @@ if(-e $dspmqpath && -e $dspmqverpath){
 	}
 }
 else {
+# there was no mq server installed there
+	print "$host,,,,";
 }
-
-
-
-
