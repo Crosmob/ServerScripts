6394512784e12a05267ffec63efdc4d34ae8aa73
Evangelos Brachos <ebrachos@gmail.com>
2015-09-01 14:03:56 +0200

    add cluster

--- eai/wmq/find_queues.pl
+++ eai/wmq/find_queues.pl
@@ -14,47 +14,50 @@ my $dspmqpath = "/opt/mqm/bin/dspmq";
 my $dspmqverpath= "/opt/mqm/bin/dspmqver";
 
 if(-e $dspmqpath && -e $dspmqverpath){
-	# mq server installation was found - now we will find the running qmgrs 
-	$dspmq = `dspmq`;
-	my @qmgrs = split /\n/, $dspmq;
-	foreach my $dspmqline (@qmgrs) {
-		if($dspmqline =~ m/\s*QMNAME\(([\w\.]+)\)\s+STATUS\((\w+)\)/) {
-			$qm = "$1";
-			$qmstatus = "$2";
-			# print "$1,$2\n";
-			# go through the running queue managers and get the queues info
-			if( $2 eq "Running"){
-				$display = `echo 'DIS Q(*) TYPE' | runmqsc $1`;
-				my @resultLines = split /\n/, $display;
-				foreach my $resultLine (@resultLines) {
-					if($resultLine =~ m/\s*QUEUE\(([\w\.]+)\)\s+TYPE\((\w+)\)/) {
-						print "$host,$qm,$qmstatus,$1,$2\n";
-					}
-					elsif($resultLine =~ m/\s*QUEUE\(([\w\.]+)\)/) {
-						print "$host,$qm,$qmstatus,$1,";
-					}
-					elsif($resultLine =~ m/\s*TYPE\((\w+)\)/) {
-						print "$1\n";
-					}
-				}
-				$displayc = `echo 'DIS QC(*) TYPE' | runmqsc $1`;
-				my @resultLinesc = split /\n/, $displayc;
-				foreach my $resultLinec (@resultLinesc) {
-					if($resultLinec =~ m/\s*QUEUE\(([\w\.]+)\)\s+TYPE\((\w+)\)/) {
-						print "$host,$qm,$qmstatus,$1,$2\n";
-					}
-					elsif($resultLinec =~ m/\s*QUEUE\(([\w\.]+)\)/) {
-						print "$host,$qm,$qmstatus,$1,";
-					}
-					elsif($resultLinec =~ m/\s*TYPE\((\w+)\)/) {
-						print "$1\n";
-					}
-				}
-			}
-		}
-	}
+        # mq server installation was found - now we will find the running qmgrs
+        $dspmq = `dspmq`;
+        my @qmgrs = split /\n/, $dspmq;
+        foreach my $dspmqline (@qmgrs) {
+                if($dspmqline =~ m/\s*QMNAME\(([\w\.]+)\)\s+STATUS\((\w+.*)\)/) {
+                        $qm = "$1";
+                        $qmstatus = "$2";
+                        # print "$1,$2\n";
+                        # go through the running queue managers and get the queues info
+                        if( $2 eq "Running"){
+                                $display = `echo 'DIS Q(*) TYPE' | runmqsc $1`;
+                                my @resultLines = split /\n/, $display;
+                                foreach my $resultLine (@resultLines) {
+                                        if($resultLine =~ m/\s*QUEUE\(([\w\.]+)\)\s+TYPE\((\w+)\)/) {
+                                                print "$host,$qm,$qmstatus,$1,$2\n";
+                                        }
+                                        elsif($resultLine =~ m/\s*QUEUE\(([\w\.]+)\)/) {
+                                                print "$host,$qm,$qmstatus,$1,";
+                                        }
+                                        elsif($resultLine =~ m/\s*TYPE\((\w+)\)/) {
+                                                print "$1\n";
+                                        }
+                                }
+                                $displayc = `echo 'DIS QC(*) TYPE' | runmqsc $1`;
+                                my @resultLinesc = split /\n/, $displayc;
+                                foreach my $resultLinec (@resultLinesc) {
+                                        if($resultLinec =~ m/\s*QUEUE\(([\w\.]+)\)\s+TYPE\((\w+)\)/) {
+                                                print "$host,$qm,$qmstatus,$1,$2\n";
+                                        }
+                                        elsif($resultLinec =~ m/\s*QUEUE\(([\w\.]+)\)/) {
+                                                print "$host,$qm,$qmstatus,$1,";
+                                        }
+                                        elsif($resultLinec =~ m/\s*TYPE\((\w+)\)/) {
+                                                print "$1\n";
+                                        }
+                                }
+                        }
+                        else{
+                                print "$host,$qm,$qmstatus,,\n";
+                        }
+                }
+        }
 }
 else {
 # there was no mq server installed there
-	print "$host,,,,";
+        print "$host,,,,\n";
 }
